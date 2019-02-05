6da8b11024a7be6d88579ab076fa447574b21ce4
Vilém Zouhar <vilem.zouhar@gmail.com>
2019-02-05 19:14:30 +0100

    v 0.0.2a; delete and select tests

--- include/share/zimadb.h
+++ include/share/zimadb.h
@@ -1,7 +1,7 @@
 #ifndef ZIMADB_H
 #define ZIMADB_H
 
-#define ZIMADB_VERSION "0.0.1c"
+#define ZIMADB_VERSION "0.0.2a"
 #define ZIMADB_DEBUG true
 
 #endif
---
+++ test/scripts/delete.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+../../bin/zimadb <<EOF
+.debug off
+.open ahoj.zima
+CREATE TABLE rostlinka (age int, name varchar);
+INSERT INTO rostlinka (age, name) values (24, "Sofie");
+INSERT INTO rostlinka (name, age) values ("Oldřich", 11);
+DELETE FROM rostlinka WHERE age = 14;
+DELETE FROM rostlinka WHERE age > 14;
+EOF
