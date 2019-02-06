68a678c4005e573ee61b69ecf367de57e36cedb3
Vilém Zouhar <vilem.zouhar@gmail.com>
2019-02-06 13:39:50 +0100

    new bug; bug reshuffle

--- test/bugs/bug-34.sh
+++ test/bugs/bug-34.sh
@@ -1,6 +1,10 @@
-.open a.zima
+#!/bin/bash
+
+../../bin/zimadb <<EOF
+open a.zima
 CREATE TABLE a (b int, c int);
 INSERT INTO a (b, c) VALUES (0, 1);
 INSERT INTO a (b, c) VALUES (2, 3);
 SELECT b, c FROM a;
 INSERT INTO a (b, c) VALUES (4, 5);
+EOF
---
+++ test/bugs/bug-35.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+../../bin/zimadb <<EOF
+.open employees.zima
+CREATE TABLE emp (emp_id INT, emp_name VARCHAR(255), emp_age INT);
+INSERT INTO emp (emp_id, emp_name, emp_age) VALUES (0, "Jarda", 20);
+EOF
