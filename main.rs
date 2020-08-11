acc3b68b454cd53f18a07a0940050412d1c70c7b
Vilém Zouhar <vilem.zouhar@gmail.com>
2020-08-11 11:05:32 +0200

    optimization

--- main.rs
+++ main.rs
@@ -9,39 +9,38 @@ const F_DOCUMENT: &str = "./data/en/kufre.txt";
 const LENGTH_POWER: f32 = 1.2;
 const DUPLICITY_SCORE: f32 = 0.1;
 
-fn create_candidates<'a>(words: &Vec<String>, sws: HashSet<String>) -> Vec<Vec<String>> {
+fn create_candidates<'a>(words: &[&str], sws: HashSet<&str>) -> Vec<Vec<String>> {
     let mut buffer: Vec<String> = Vec::new();
     let mut candidates: Vec<Vec<String>> = Vec::new();
 
-    let uppercase_regex = Regex::new(r"\p{Lu}.+").unwrap();
-    let word_regex = Regex::new(r"\pL+").unwrap();
+    let r_uppercase = Regex::new(r"\p{Lu}.+").unwrap();
+    let r_word = Regex::new(r"\pL+").unwrap();
 
     for word in words {
-        if !word_regex.is_match(word) {
+        if !r_word.is_match(word) {
             continue;
         }
 
-        if sws.contains(&word.to_lowercase()) {
+        if sws.contains(&word.to_lowercase().as_str()) {
             if buffer.len() != 0 {
                 candidates.push(buffer);
                 buffer = Vec::new();
             }
-        } else if uppercase_regex.is_match(word) {
+        } else if r_uppercase.is_match(word) {
             if buffer.len() != 0 {
                 candidates.push(buffer);
                 buffer = Vec::new();
             }
-            buffer.push(word.to_lowercase().to_string());
+            buffer.push(word.to_lowercase());
         } else {
-            buffer.push(word.to_lowercase().to_string());
+            buffer.push(word.to_lowercase());
         }
     }
-
 
     return candidates;
 }
 
-fn crf_key<'a>(w1: &String, w2: &String) -> String {
+fn crf_key<'a>(w1: &str, w2: &str) -> String {
     let key: String;
     if w1 < w2 {
         key = format!("{}-{}", w1, w2);
@@ -51,35 +50,30 @@ fn crf_key<'a>(w1: &String, w2: &String) -> String {
     return key;
 }
 
-fn compute_crf_frq_deg<'a, 'b>(
-    candidates: &Vec<Vec<String>>,
-) -> (
-    HashMap<String, u32>,
-    HashMap<String, u32>,
-    HashMap<String, u32>,
-) {
+fn compute_crf_frq_deg<'a>(
+    candidates: &[Vec<String>],
+) -> (HashMap<String, u32>, HashMap<String, u32>, HashMap<String, u32>) {
     let mut cfs: HashMap<String, u32> = HashMap::new();
     let mut frq: HashMap<String, u32> = HashMap::new();
     let mut deg: HashMap<String, u32> = HashMap::new();
-    let mut deg_array: HashMap<String, HashSet<String>> = HashMap::new();
+    let mut deg_array: HashMap<String, HashSet<&str>> = HashMap::new();
 
     for set in candidates {
         for w1 in set {
-            if !deg_array.contains_key(w1) {
-                deg_array.insert(w1.to_string(), HashSet::new());
+            if !deg_array.contains_key(w1.as_str()) {
+                deg_array.insert(w1.clone(), HashSet::new());
             }
 
-            let val: u32 = *frq.get(w1).unwrap_or(&0);
-            frq.insert(w1.to_string(), val + 1);
+            let val: u32 = *frq.get(w1.as_str()).unwrap_or(&0);
+            frq.insert(w1.clone(), val + 1);
 
             for w2 in set {
                 if w2 > w1 {
-                    let key: String = crf_key(&w1, &w2);
+                    let key: String = crf_key(w1, w2);
                     let val: u32 = *cfs.get(&key).unwrap_or(&0);
                     cfs.insert(key, val + 1);
                 }
-                
-                deg_array.get_mut(w1).unwrap().insert(w2.to_string());
+                deg_array.get_mut(w1.as_str()).unwrap().insert(w2);
             }
         }
     }
@@ -94,45 +88,40 @@ fn compute_crf_frq_deg<'a, 'b>(
 fn main() {
     println!("Opening stopwords from '{}'", F_STOPWORDS);
     let sws_raw: String = fs::read_to_string(F_STOPWORDS).expect("The file could not be opened.");
-    let sws: HashSet<String> = HashSet::from_iter(sws_raw.lines().map(|x| x.to_string()));
+    let sws: HashSet<&str> = HashSet::from_iter(sws_raw.lines());
 
     println!("Opening document from '{}'", F_DOCUMENT);
-    let doc_raw = fs::read_to_string(F_DOCUMENT).expect("The file could not be opened.");
+    let doc_raw: String = fs::read_to_string(F_DOCUMENT).expect("The file could not be opened.");
 
     println!("");
 
-    let doc_words: Vec<String> = Regex::new(r"\W+")
-        .unwrap()
-        .split(&doc_raw.to_owned())
-        .map(|x| x.to_string())
-        .collect();
-
+    let doc_words: Vec<&str> = Regex::new(r"\W+").unwrap().split(&doc_raw).collect();
     let candidates = create_candidates(&doc_words, sws);
 
     let (_cfrs, frqs, degs) = compute_crf_frq_deg(&candidates);
-    
-    let mut rat : HashMap<String, f32> = HashMap::new();
+
+    let mut rat: HashMap<String, f32> = HashMap::new();
     for (key, f_val) in frqs {
         let d_val = degs.get(&key).unwrap().clone() as f32;
-        rat.insert(key, d_val/(f_val as f32));
+        rat.insert(key, d_val / (f_val as f32));
     }
-    
-    let mut keywords : HashMap<String, f32> = HashMap::new();
+
+    let mut keywords: HashMap<String, f32> = HashMap::new();
     for candidate in candidates {
         let mut sum: f32 = 0.0;
         let length: u32 = candidate.len() as u32;
-        let key : String = candidate.join(" ");
+        let key: String = candidate.join(" ");
         for word in &candidate {
-            sum += rat.get(word).unwrap();
+            sum += rat.get(word.as_str()).unwrap();
         }
-        let mut score = sum/f32::powf(length as f32, LENGTH_POWER);
-        if keywords.contains_key(&key) {
+        let mut score = sum / f32::powf(length as f32, LENGTH_POWER);
+        if keywords.contains_key(key.as_str()) {
             score += DUPLICITY_SCORE;
         }
         keywords.insert(key, score);
     }
 
-    let mut keyword_vec:Vec<(&String, &f32)> = keywords.iter().collect();
+    let mut keyword_vec: Vec<(&str, &f32)> = keywords.iter().map(|(k,v)| (k.as_str(), v)).collect();
     keyword_vec.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
 
     println!("Score  Keyword");
