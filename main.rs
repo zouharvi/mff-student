d9cafc481eba7914997303331c9cacf4cda8ccfb
Vilém Zouhar <vilem.zouhar@gmail.com>
2020-08-12 09:01:53 +0200

    add idf, glob all hulth2003

--- main.rs
+++ main.rs
@@ -1,15 +1,46 @@
+use glob::glob;
 use regex::Regex;
-use std::collections::HashMap;
-use std::collections::HashSet;
+use std::collections::{HashMap,HashSet};
 use std::fs;
 use std::iter::FromIterator;
+use std::f32;
+use std::path::PathBuf;
+
 
 const F_STOPWORDS: &str = "./data/en/stopwords.txt";
-const HULTH_ID: u32 = 1139;
+const G_ABSTR: &str = "./data/hulth2003/*.abstr";
 const LENGTH_POWER: f32 = 1.2;
-const DUPLICITY_SCORE: f32 = 0.1;
+const DUPLICITY_SCORE: f32 = 0.5;
 
-fn create_candidates<'a>(words: &[&str], sws: HashSet<&str>) -> Vec<Vec<String>> {
+fn read_all(sws: &HashSet<&str>) -> Vec<HashSet<String>> {
+    let mut data = Vec::new();
+
+    let r_not_word = Regex::new(r"[^\pL]").unwrap();
+
+    for entry in glob(G_ABSTR).expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => {
+                let text = fs::read_to_string(path).expect("The file could not be opened.").to_lowercase();
+                let set = r_not_word.split(&text).map(|x| x.to_string()).filter(|x| !sws.contains(x.as_str())).collect::<HashSet<String>>();
+                data.push(set);
+            }
+            Err(e) => println!("{:?}", e),
+        }
+    }
+    return data;
+}
+
+fn idf(data: &Vec<HashSet<String>>, term: &str) -> f32 {
+    let mut sum = 0;
+    for doc in data {
+        if doc.contains(term) {
+            sum += 1;
+        }
+    }
+    return (data.len() as f32).log(2.0)/(sum as f32);
+}
+
+fn create_candidates<'a>(words: &[&str], sws: &HashSet<&str>) -> Vec<Vec<String>> {
     let mut buffer: Vec<String> = Vec::new();
     let mut candidates: Vec<Vec<String>> = Vec::new();
 
@@ -90,26 +121,41 @@ fn compute_crf_frq_deg<'a>(
 }
 
 fn main() {
-    let F_DOCUMENT: String = format!("./data/hulth2003/{}.abstr", HULTH_ID);
-    let F_UNCONTR: String = format!("./data/hulth2003/{}.uncontr", HULTH_ID);
-
     println!("Opening stopwords from '{}'", F_STOPWORDS);
     let sws_raw: String = fs::read_to_string(F_STOPWORDS).expect("The file could not be opened.");
     let sws: HashSet<&str> = HashSet::from_iter(sws_raw.lines());
+    
+    println!("Reading all from '{}'", G_ABSTR);
+    let data = read_all(&sws);
 
-    println!("Opening document from '{}'", F_DOCUMENT);
-    let doc_raw: String = fs::read_to_string(F_DOCUMENT).expect("The file could not be opened.");
+    for entry in glob(G_ABSTR).expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => {
+                let mut uncontr_path = PathBuf::from(&path);
+                uncontr_path.set_extension("uncontr");
+                process_abstr(&data, &sws, path.as_os_str().to_str().unwrap(), uncontr_path.as_os_str().to_str().unwrap());
+            }
+            Err(e) => println!("{:?}", e),
+        }
+    }
+}
 
-    let mut uncontr_raw: String = fs::read_to_string(F_UNCONTR).expect("The file could not be opened.");
+fn process_abstr(data: &Vec<HashSet<String>>, sws: &HashSet<&str>, f_abstr: &str, f_uncontr: &str) {
+    println!("\nOpening document from '{}'", f_abstr);
+    let doc_raw: String = fs::read_to_string(f_abstr).expect("The file could not be opened.");
+
+    println!("Opening uncontr from '{}'", f_uncontr);
+    let mut uncontr_raw: String =
+        fs::read_to_string(f_uncontr).expect("The file could not be opened.");
     uncontr_raw = uncontr_raw.replace("\n", "");
     uncontr_raw = uncontr_raw.replace("\t", "");
     uncontr_raw = uncontr_raw.replace("\r", "");
-    let uncontr :HashSet<&str> = HashSet::from_iter(uncontr_raw.split("; ").collect::<Vec<&str>>());
+    let uncontr: HashSet<String> = HashSet::from_iter(uncontr_raw.split("; ").map(|x| x.to_string()).collect::<Vec<String>>());
 
     println!("");
 
     let doc_words: Vec<&str> = Regex::new(r"\W+").unwrap().split(&doc_raw).collect();
-    let candidates = create_candidates(&doc_words, sws);
+    let candidates = create_candidates(&doc_words, &sws);
 
     let (_cfrs, frqs, degs) = compute_crf_frq_deg(&candidates);
 
@@ -125,7 +171,7 @@ fn main() {
         let length: u32 = candidate.len() as u32;
         let key: String = candidate.join(" ");
         for word in &candidate {
-            sum += rat.get(word.as_str()).unwrap();
+            sum += rat.get(word.as_str()).unwrap() * idf(&data, word);
         }
         let mut score = sum / f32::powf(length as f32, LENGTH_POWER);
         if keywords.contains_key(key.as_str()) {
@@ -138,10 +184,13 @@ fn main() {
         keywords.iter().map(|(k, v)| (k.as_str(), v)).collect();
     keyword_vec.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
 
-    println!("Score  Keyword");
-    for n in 1..20 {
+    println!("Score  Keyword {}", f_abstr);
+    for n in 1..15 {
+        if n >= keyword_vec.len() {
+            break;
+        }
         let (key, val) = keyword_vec.get(n).unwrap();
-        if uncontr.contains(key) {
+        if uncontr.contains(*key) {
             println!("* {:.3}: {}", val, key);
         } else {
             println!("  {:.3}: {}", val, key);
